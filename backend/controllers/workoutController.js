858ae739ea2b77d80020af69a614fa9182634944
gabster <gabster@yolo.com>
2023-10-18 15:18:09 +0800

    working up to add new workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -28,14 +28,13 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
 
-  //add doc to bd
+  // add to the database
   try {
     const workout = await Workout.create({ title, load, reps });
     res.status(200).json(workout);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-  res.json({ mssg: "POST a new workout" });
 };
 
 // delete a workout
