622c11a959d9b636553b6cf179db8ee55ff836e5
gabster <gabster@yolo.com>
2023-10-18 15:32:38 +0800

    reloads and updates after addition of workout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
+// components
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -12,12 +14,12 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json);
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
 
     fetchWorkouts();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="home">
@@ -25,7 +27,6 @@ const Home = () => {
         {workouts &&
           workouts.map((workout) => (
             <WorkoutDetails workout={workout} key={workout._id} />
-            //<p key={workout._id}>{workout.title}</p>
           ))}
       </div>
       <WorkoutForm />
