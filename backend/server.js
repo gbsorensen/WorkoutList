db2ed2f16af88d58093aff6509c7dc45930a42b6
gabster <gabster@yolo.com>
2023-10-18 11:12:07 +0800

    finished backend folder

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+//starting the express app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/workouts", workoutRoutes);
+
+// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(`connected to db and listening to port`, process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
